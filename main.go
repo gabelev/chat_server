d362cc879a3744b2db5e0acd02b1519ebad5f678
Gabriel Levine <gabelev@gmail.com>
2016-08-28 09:46:14 -0400

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
@@ -11,21 +7,11 @@ import (
 	"text/template"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var addr = flag.String("addr", ":9000", "http service address")
 var homeTemplate = template.Must(template.ParseFiles("home.html"))
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTemplate.Execute(w, r.Host)
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
 
 func main() {
