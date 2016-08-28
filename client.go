f5d43f842ae675cd48b1bf3e360396b1aba15299
Gabriel Levine <gabelev@gmail.com>
2016-08-28 12:52:35 -0400

    added websocket origin

--- client.go
+++ client.go
@@ -1,7 +1,3 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
@@ -33,6 +29,7 @@ var (
 )
 
 var upgrader = websocket.Upgrader{
+	CheckOrigin:     func(r *http.Request) bool { return true },
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
